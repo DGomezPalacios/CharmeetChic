3d4d234f8eefaf681ecf053c5e606fad08bb3bd4
DGomezPalacios <da.gomezp@duocuc.cl>
2025-05-16 22:40:25 -0400

    base proyecto

---
+++ src/main/java/com/charmeetchic/CharmeetChic/CharmeetChicApplication.java
@@ -0,0 +1,13 @@
+package com.charmeetchic.CharmeetChic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharmeetChicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharmeetChicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/charmeetchic/CharmeetChic/CharmeetChicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charmeetchic.CharmeetChic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharmeetChicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

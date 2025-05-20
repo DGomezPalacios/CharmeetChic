5d55e8cf411d0663a466e6ba1efd99cbd50afa91
DGomezPalacios <da.gomezp@duocuc.cl>
2025-05-19 22:54:09 -0400

    Usuario modelo y repository

--- src/main/java/com/charmeetchic/CharmeetChic/repository/UsuarioRepository.java
+++ src/main/java/com/charmeetchic/CharmeetChic/repository/UsuarioRepository.java
@@ -1,5 +1,13 @@
 package com.charmeetchic.CharmeetChic.repository;
 
-public class UsuarioRepository {
+import java.util.Optional;
+
+import com.charmeetchic.CharmeetChic.model.Usuario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findbyCorreo(String correo);
 
 }

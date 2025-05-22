1da86c6516d2221e0855bddc5a3b87c6245dc431
DGomezPalacios <da.gomezp@duocuc.cl>
2025-05-22 18:18:25 -0400

    simplificar importaciones

--- src/main/java/com/charmeetchic/CharmeetChic/controller/CategoriaController.java
+++ src/main/java/com/charmeetchic/CharmeetChic/controller/CategoriaController.java
@@ -3,11 +3,8 @@ package com.charmeetchic.CharmeetChic.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 import com.charmeetchic.CharmeetChic.model.Categoria;
 import com.charmeetchic.CharmeetChic.service.CategoriaService;
